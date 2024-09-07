344c02b2c4f0cc38c9f941727715f879223ffc13
owusu kelvin clark (lokaimoma) <kelvinclark@lokaitech.com>
2024-09-07 21:40:31 +0100

    Initialize project

---
+++ src/main/java/com/koc/studai/StudaiApplication.java
@@ -0,0 +1,13 @@
+package com.koc.studai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koc/studai/StudaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koc.studai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
