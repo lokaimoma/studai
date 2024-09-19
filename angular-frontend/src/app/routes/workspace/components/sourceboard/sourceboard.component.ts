de369fdbca844b09cf128ec8413f2ab60b0b6ea2
owusu kelvin clark (lokaimoma) <kelvinclark@lokaitech.com>
2024-09-19 09:49:22 +0100

    Add spartan ui diaglog component

--- angular-frontend/src/app/routes/workspace/components/sourceboard/sourceboard.component.ts
+++ angular-frontend/src/app/routes/workspace/components/sourceboard/sourceboard.component.ts
@@ -3,11 +3,17 @@ import { IconsModule } from '../../../../icons/icons.module';
 import { HlmCheckboxComponent } from '@spartan-ng/ui-checkbox-helm';
 import { SourceComponent } from './components/source/source.component';
 import { Source } from '../../../../types';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-sourceboard',
   standalone: true,
-  imports: [IconsModule, HlmCheckboxComponent, SourceComponent],
+  imports: [
+    IconsModule,
+    HlmCheckboxComponent,
+    SourceComponent,
+    HlmButtonDirective,
+  ],
   templateUrl: './sourceboard.component.html',
 })
 export class SourceboardComponent {
