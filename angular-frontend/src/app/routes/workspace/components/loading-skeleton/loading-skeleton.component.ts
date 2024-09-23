ec2278a2b82d80d3c629178536e7549331e0032b
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2024-09-23 14:45:35 +0100

    Remove angular app

--- angular-frontend/src/app/routes/workspace/components/loading-skeleton/loading-skeleton.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading-skeleton',
-  standalone: true,
-  imports: [],
-  templateUrl: './loading-skeleton.component.html',
-})
-export class LoadingSkeletonComponent {}
