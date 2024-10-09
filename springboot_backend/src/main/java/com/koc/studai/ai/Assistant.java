f5efe42a5857df935dfb2a5e1a9c7c62911f69b0
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2024-10-09 08:46:37 +0100

    Add system prompt

--- springboot_backend/src/main/java/com/koc/studai/ai/Assistant.java
+++ springboot_backend/src/main/java/com/koc/studai/ai/Assistant.java
@@ -1,10 +1,12 @@
 package com.koc.studai.ai;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 
 public interface Assistant {
 	static final String USERiD_WORKSPACEID_SEP = ":#:";
 	// userAndWorkspaceId = userId + USERiD_WORKSPACEID_SEP + workspaceId 
+    @SystemMessage("You're a tutor with access to your student's documents. If no document is provided to you for a query. Respond with no document found relating to this query")
 	String chat(@MemoryId String userAndWorkspaceId, @UserMessage String message);
 }
